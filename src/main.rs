6d9490cf372812c5aa3961e0d0fd3664c6b477c0
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-04-07 19:35:44 +0200

    Update template

--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,44 @@
 use yew::prelude::*;
 
-#[function_component(App)]
-fn app() -> Html {
-    html! {
-        <p class={"bg-blue-100"}>{"Test!"}</p>
+enum Msg {
+    AddOne,
+}
+
+struct Model {
+    value: i64,
+}
+
+impl Component for Model {
+    type Message = Msg;
+    type Properties = ();
+
+    fn create(_ctx: &Context<Self>) -> Self {
+        Self { value: 0 }
+    }
+
+    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+        match msg {
+            Msg::AddOne => {
+                self.value += 1;
+                // the value has changed so we need to
+                // re-render for it to appear on the page
+                true
+            }
+        }
+    }
+
+    fn view(&self, ctx: &Context<Self>) -> Html {
+        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
+        let link = ctx.link();
+        html! {
+            <div>
+                <button class="bg-green-100 p-2 rounded-full" onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
+                <p>{ self.value }</p>
+            </div>
+        }
     }
 }
 
 fn main() {
-    yew::start_app::<App>();
+    yew::start_app::<Model>();
 }
