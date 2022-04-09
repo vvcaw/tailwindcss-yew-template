f20297fb12a87aaae8edabb53af932ef24b5b3db
Eric Elflein <57096338+vvcaw@users.noreply.github.com>
2022-04-09 21:32:08 +0200

    Better example app

--- src/main.rs
+++ src/main.rs
@@ -1,44 +1,100 @@
+extern crate reqwest_wasm;
+
+use std::borrow::Borrow;
+use yew::html::Scope;
 use yew::prelude::*;
 
+mod data;
+
 enum Msg {
-    AddOne,
+    FetchNewUser,
+    UpdatePerson(data::Result),
 }
 
-struct Model {
-    value: i64,
+struct App {
+    user: data::Result,
 }
 
-impl Component for Model {
+impl Component for App {
     type Message = Msg;
     type Properties = ();
 
-    fn create(_ctx: &Context<Self>) -> Self {
-        Self { value: 0 }
+    fn create(ctx: &Context<Self>) -> Self {
+        Self {
+            user: data::Result::default()
+        }
     }
 
-    fn update(&mut self, _ctx: &Context<Self>, msg: Self::Message) -> bool {
+    fn update(&mut self, ctx: &Context<Self>, msg: Self::Message) -> bool {
+        let link = ctx.link();
+
         match msg {
-            Msg::AddOne => {
-                self.value += 1;
-                // the value has changed so we need to
-                // re-render for it to appear on the page
+            Msg::FetchNewUser => {
+                link.send_future(App::get_person());
+
+                false
+            }
+            Msg::UpdatePerson(person) => {
+                log::info!("Update Person: {:?}", {&person});
+                self.user = person;
+
                 true
             }
         }
     }
 
     fn view(&self, ctx: &Context<Self>) -> Html {
-        // This gives us a component's "`Scope`" which allows us to send messages, etc to the component.
-        let link = ctx.link();
+        let onclick = ctx.link().callback(|_| Msg::FetchNewUser);
+
         html! {
-            <div>
-                <button class="bg-green-100 p-2 rounded-full" onclick={link.callback(|_| Msg::AddOne)}>{ "+1" }</button>
-                <p>{ self.value }</p>
+            <div class="h-screen bg-gray-600 w-full flex flex-col items-center justify-center gap-y-4">
+                <div class="w-96 h-80 bg-gray-800 shadow-md border-indigo-400 h-auto w-auto p-4 pl-8 pr-8 rounded-md font-medium text-xl flex flex-col items-center">
+                    <img class="rounded-full w-24 h-24" src={self.user.picture.large.to_string()} />
+                    <div class="mt-4 mb-4 flex flex-col gap-y-1">
+                        <div class="flex flex-row">
+                            <span class="text-gray-100 font-medium">{"Gender: "}</span>
+                            <span class="text-gray-300 font-light ml-2">{&self.user.gender}</span>
+                        </div>
+                        <div class="flex flex-row">
+                            <span class="text-gray-100 font-medium">{"Registered: "}</span>
+                            <span class="text-gray-300 font-light ml-2">{&self.user.registered.date}</span>
+                        </div>
+                        <div class="flex flex-row">
+                            <span class="text-gray-100 font-medium">{"Phone number: "}</span>
+                            <span class="text-gray-300 font-light ml-2">{&self.user.phone}</span>
+                        </div>
+                    </div>
+                    <span class="font-bold text-xl text-center text-indigo-400"> {&self.user.name.first} {" "} {&self.user.name.last} </span>
+                    <span class="font-light text-lg text-center text-gray-400"> {"Password: "} {&self.user.login.password} </span>
+                </div>
+
+                <button {onclick} class="bg-indigo-400 shadow-md h-auto w-auto pl-4 pr-4 pb-2 pt-2 rounded-md font-medium text-xl text-white hover:bg-indigo-300">{"Find date"}</button>
             </div>
+        }
+    }
+
+    fn rendered(&mut self, ctx: &Context<Self>, first_render: bool) {
+        if first_render {
+            ctx.link().send_future(App::get_person());
         }
     }
 }
 
+impl App {
+    async fn get_person() -> Msg {
+        let res = reqwest_wasm::get("https://randomuser.me/api/1.2")
+            .await
+            .unwrap()
+            .text()
+            .await
+            .unwrap();
+
+        let parsed_json = serde_json::from_str::<data::Root>(res.as_str()).unwrap();
+        return Msg::UpdatePerson((*parsed_json.results.first().unwrap()).clone());
+    }
+}
+
 fn main() {
-    yew::start_app::<Model>();
+    wasm_logger::init(wasm_logger::Config::default());
+    yew::start_app::<App>();
 }
